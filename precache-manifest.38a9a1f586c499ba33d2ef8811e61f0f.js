55ddd08a2da30db5fafb412186e37246ea81eee3
Mohammed Uves <mohammed.uves93@gmail.com>
2018-11-10 06:38:59 +0530

    Updates

---
+++ precache-manifest.38a9a1f586c499ba33d2ef8811e61f0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "d0b343dc8e46eabd0f5d",
+    "url": "/contactmanager/static/js/main.d0b343dc.chunk.js"
+  },
+  {
+    "revision": "0a0abdaf05750855d8d4",
+    "url": "/contactmanager/static/js/1.0a0abdaf.chunk.js"
+  },
+  {
+    "revision": "0a0abdaf05750855d8d4",
+    "url": "/contactmanager/static/css/1.7c9d6509.chunk.css"
+  },
+  {
+    "revision": "f3fa2a2c2657982805efa59b9302e2f8",
+    "url": "/contactmanager/index.html"
+  }
+];
